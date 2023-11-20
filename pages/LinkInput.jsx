1f935dd3ed7d7239b1d9404fb9d2d70547ad88fc
techno absurdist <92827957+technoabsurdist@users.noreply.github.com>
2023-11-20 15:29:50 -0800

    more UI stuff with dummy data

--- pages/LinkInput.jsx
+++ pages/LinkInput.jsx
@@ -1,10 +1,39 @@
+import React, { useState } from 'react';
 import styles from './LinkInput.module.css';
 
 export const LinkInput = () => {
+    const [link, setLink] = useState('');
+    const [response, setResponse] = useState('');
+
+    const submitLink = () => {
+        setTimeout(() => {
+            const dummyResponse = `Processed link: ${link}`;
+            setResponse(dummyResponse);
+        }, 1000); 
+    };
+
     return (
-        <div className={styles.container}>
-            <input type="text" className={styles.inputBar} placeholder="Enter youtube link" /> 
-            <button type="submit" className={styles.submitButton}>Submit</button>
+        <>
+         <div className={styles.container}>
+            <input 
+                type="text" 
+                className={styles.inputBar} 
+                placeholder="Enter youtube link"
+                value={link}
+                onChange={(e) => setLink(e.target.value)}
+            /> 
+            <button 
+                type="button" 
+                onClick={submitLink} 
+                className={styles.submitButton}
+            >
+                Submit
+            </button>
         </div>
-    )
+        <div>
+            {response && <div className={styles.response}>{response}</div>}
+        </div>
+        
+        </>
+   );
 }
